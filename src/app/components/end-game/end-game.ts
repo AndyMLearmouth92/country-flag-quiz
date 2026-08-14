209b1ec2e533979197001a50931444b7a1922c09
Andy <andymlearmouth@hotmail.com>
2026-08-15 00:00:00 +0100

    Built the quizEndMessage function to provide a message to the user at the end of the quiz.

--- src/app/components/end-game/end-game.ts
+++ src/app/components/end-game/end-game.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Heading } from '../heading/heading';
 import { Button } from '../button/button';
+import { Quiz } from '../../services/quiz/quiz';
 
 @Component({
   selector: 'app-end-game',
@@ -8,4 +9,23 @@ import { Button } from '../button/button';
   templateUrl: './end-game.html',
   styleUrl: './end-game.css',
 })
-export class EndGame {}
+export class EndGame {
+  quizService = inject(Quiz)
+  score = this.quizService.correctlyAnsweredQuestions()
+  
+  quizEndMessage() {
+    if (this.score < 3) {
+      return 'Better luck next time.';
+    }
+    if (this.score < 6) {
+      return 'Ok effort, but more practice needed.';
+    }
+    if (this.score < 8) {
+      return 'Good effort.';
+    }
+    if (this.score < 10) {
+      return 'Great effort.';
+    }
+    return 'Perfect score!';
+  }
+}
